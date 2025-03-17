cf707e22fe76781888ac3f278a350005df61ca59
minuwegdam <minuwegdam@gmail.com>
2025-03-17 06:31:43 +0000

    Add 12.php

---
+++ src/12.php
@@ -0,0 +1,13 @@
+<?php
+// Define an array of numbers
+$numbers = array(1, 2, 3, 4, 5);
+
+// Use a loop to sum up all the numbers in the array
+$sum = 0;
+foreach ($numbers as $number) {
+    $sum += $number;
+}
+
+// Print the result
+echo "The sum of the numbers is: $sum";
+?>
