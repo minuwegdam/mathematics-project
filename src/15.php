a9bda8291cf1ba96727708586454b1d41cc848ac
minuwegdam <minuwegdam@gmail.com>
2025-03-21 08:39:34 +0000

    Add 15.php

---
+++ src/15.php
@@ -0,0 +1,11 @@
+<?php
+// Define some constants
+define('MY_CONSTANT', 10);
+
+// Access the constant from another file
+constant = MY_CONSTANT;
+
+// Use the constant in your PHP code
+
+echo "This is a constant used in my project: $constant";
+?>
