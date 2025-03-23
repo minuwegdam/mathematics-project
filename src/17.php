6f384cbb501241e71515c3316fc54306f2e92081
minuwegdam <minuwegdam@gmail.com>
2025-03-23 08:36:44 +0000

    Add 17.php

---
+++ src/17.php
@@ -0,0 +1,23 @@
+<?php
+class MathOperations {
+    public function add($a, $b) {
+        return $a + $b;
+    }
+
+    public function subtract($a, $b) {
+        return $a - $b;
+    }
+
+    public function multiply($a, $b) {
+        return $a * $b;
+    }
+
+    public function divide($a, $b) {
+        if ($b != 0) {
+            return $a / $b;
+        } else {
+            throw new Exception("Division by zero is not allowed.");
+        }
+    }
+}
+?>
