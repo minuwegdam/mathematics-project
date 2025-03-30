dfc8dbdfae27320f2a34df5b8402819140d838b9
minuwegdam <minuwegdam@gmail.com>
2025-03-30 14:58:21 +0000

    Add 20.php

---
+++ src/20.php
@@ -0,0 +1,10 @@
+<?php
+// Define variables
+$student = "John";
+$email = "johndoe@example.com";
+
+// Output variables to HTML page
+echo "<h1>Student Information</h1>";
+echo "<p>Name: $student</p>";
+echo "<p>Email: $email</p>";
+?>
