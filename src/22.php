fe77def8a52d71f19d47d075e4c4e173a014c865
minuwegdam <minuwegdam@gmail.com>
2025-04-01 18:57:56 +0000

    Add 22.php

---
+++ src/22.php
@@ -0,0 +1,10 @@
+<?php
+// Define an array of numbers to be sorted
+$numbers = [1, 3, 5, 7, 9];
+
+// Use the sort() function to sort the array in ascending order
+sort($numbers);
+
+// Output the sorted array
+print_r($numbers);
+?>
