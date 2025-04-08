479b95f9f4fcd6e4e5e43e1e14ee607e1c00830e
minuwegdam <minuwegdam@gmail.com>
2025-04-08 11:19:11 +0000

    Add 28.php

---
+++ src/28.php
@@ -0,0 +1,12 @@
+<?php
+// Define variables and constants
+$var1 = "Variable 1";
+$var2 = "Variable 2";
+
+// Check if both variables are assigned
+if (isset($var1) && isset($var2)) {
+    echo "Both variables have been set.";
+} else {
+    echo "At least one variable is not set.";
+}
+?>
