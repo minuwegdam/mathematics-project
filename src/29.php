87d2d8f7ff342284045095bbb480be05bde32f67
minuwegdam <minuwegdam@gmail.com>
2025-04-09 04:26:54 +0000

    Add 29.php

---
+++ src/29.php
@@ -0,0 +1,11 @@
+<?php
+// Define some constants
+const PI = 3.14;
+const RADIUS = 5;
+
+// Calculate area of circle
+$area = PI * pow(RADIUS, 2);
+
+// Print area
+echo "The area of the circle is: $area";
+?>
