69d8384f8db43e8c196d98bd26854022ccfa6c2c
minuwegdam <minuwegdam@gmail.com>
2025-04-26 03:24:16 +0000

    Add 34.php

---
+++ src/34.php
@@ -0,0 +1,22 @@
+<?php
+// Example PHP code for exploring mathematical concepts through coding
+// Here we'll play with some basic arithmetic and algebraic expressions
+
+// Adding 2 to 3
+echo "2 + 3 = " . (2 + 3) . "<br>";
+
+// Subtracting 5 from 8
+echo "8 - 5 = " . (8 - 5) . "<br>";
+
+// Multiplying 4 by 6
+echo "4 * 6 = " . (4 * 6) . "<br>";
+
+// Dividing 12 by 3
+echo "12 / 3 = " . (12 / 3) . "<br>";
+
+// Adding 7 to 9 and dividing the result by 2
+echo "7 + 9 = " . ((7 + 9) / 2) . "<br>";
+
+// Subtracting 4 from 6, then multiplying the result by 5
+echo "6 - 4 * 5 = " . (((6 - 4) * 5)) . "<br>";
+?>
