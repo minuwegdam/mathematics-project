55dedc9097aba373a431b92744c9b20556b4ef4d
minuwegdam <minuwegdam@gmail.com>
2025-05-20 03:56:40 +0000

    Add 40.php

---
+++ src/40.php
@@ -0,0 +1,16 @@
+<?php
+
+// Function to multiply two numbers
+function multiply($a, $b) {
+    return $a * $b;
+}
+
+// Main function
+function main() {
+    // Example usage of multiply function
+    echo "The result of 3 multiplied by 4 is: " . multiply(3, 4);
+}
+
+// Execute the main function
+main();
+?>
