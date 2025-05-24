54aa2f099302a77150e382b89e9fd86b2e502c39
minuwegdam <minuwegdam@gmail.com>
2025-05-24 23:47:10 +0000

    Add 42.php

---
+++ src/42.php
@@ -0,0 +1,12 @@
+<?php
+// This is an example PHP script demonstrating how to use arrays and loops.
+// In this script, we create an array of numbers from 0 to 9 and then loop through it.
+
+$numbers = range(0, 9);
+
+foreach ($numbers as $number) {
+    echo "Number: " . $number . "<br>";
+}
+
+// If you want to add a specific number or perform some operation with the array,
+// simply replace 'range' with your desired function and modify the loop accordingly.
