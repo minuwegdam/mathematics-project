23b964df0069aae52c8d7cd46bb251532cf8c45a
minuwegdam <minuwegdam@gmail.com>
2025-05-26 22:17:01 +0000

    Add 47.php

---
+++ src/47.php
@@ -0,0 +1,19 @@
+<?php
+// Define variables and constants
+$var1 = "value1";
+$var2 = "value2";
+
+// Check if variable $var1 is set or not
+if (isset($var1)) {
+    echo "Variable $var1 has been set.";
+} else {
+    echo "Variable $var1 is not set.";
+}
+
+// Check if variable $var2 is set or not
+if (isset($var2)) {
+    echo "Variable $var2 has been set.";
+} else {
+    echo "Variable $var2 is not set.";
+}
+?>
