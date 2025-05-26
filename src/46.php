1e2f56db157ddd61d0a1a5134682583e14388c88
minuwegdam <minuwegdam@gmail.com>
2025-05-26 17:49:54 +0000

    Add 46.php

---
+++ src/46.php
@@ -0,0 +1,5 @@
+<?php
+// This is a simple PHP script to demonstrate the use of arrays and basic operations.
+$numbers = array(1, 2, 3, 4, 5);
+echo "The sum of the numbers in the array: " . array_sum($numbers) . "\n";
+?>
